6649932c985f62422f2fcc3f9610a8deb9e2b640
atiato78 <40871759+atiato78@users.noreply.github.com>
2019-07-21 16:23:49 +0300

    put 500

--- spring/src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ spring/src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -266,9 +266,9 @@ public class Application extends SpringBootServletInitializer {
                }
             })
             .delay(10000)
+            .log("Inserted new sid ${body}")
          //   .log("Inserted new sid ${body}")
-         //   .log("Inserted new sid ${body}")
-         //  .delay(500)
+          .delay(500)
             .log("Inserted out of proecedure order ${header.span_out}");
 
         }
