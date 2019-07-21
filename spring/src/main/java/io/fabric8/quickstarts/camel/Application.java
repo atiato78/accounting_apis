d7c234b6c1ae31eef279bf31d38f2f1a509d1891
atiato78 <40871759+atiato78@users.noreply.github.com>
2019-07-21 15:45:08 +0300

    rebase again

--- spring/src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ spring/src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -267,6 +267,7 @@ public class Application extends SpringBootServletInitializer {
             })
           //  .delay(10000)
             .log("Inserted new sid ${body}")
+            .log("Inserted new sid ${body}")
 
             .log("Inserted out of proecedure order ${header.span_out}");
 
