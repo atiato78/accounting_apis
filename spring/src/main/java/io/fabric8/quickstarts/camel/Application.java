76d2ab25efb7c831e462fbbb64878c60f9e3d5e6
atiato78 <40871759+atiato78@users.noreply.github.com>
2019-07-21 15:20:01 +0300

    Update successes

--- spring/src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ spring/src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -266,6 +266,7 @@ public class Application extends SpringBootServletInitializer {
                }
             })
           //  .delay(10000)
+            .log("inserted time ${header._time}")
             .log("Inserted out of proecedure order ${header.span_out}");
 
         }
